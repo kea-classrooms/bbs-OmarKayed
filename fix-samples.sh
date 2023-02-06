47028b2307204da0c1abd7e82977e84536492218
OmarKayed <113174148+OmarKayed@users.noreply.github.com>
2023-02-06 13:21:51 +0000

    Fixes

--- fix-samples.sh
+++ fix-samples.sh
@@ -1,7 +1,8 @@
-cd samples/animals
-mv countries ..
-
-cd ..
-
-cd countries/
-mv movies ..
+cd samples/
+mv animals/countries/ .
+mv countries/movies/ .
+mv animals/Mmonkies.txt animals/Monkeys.txt
+echo Monkeys > animals/Monkeys.txt
+mv movies/Germany.txt countries/
+mv countries/.apes.txt animals/
+cd -
